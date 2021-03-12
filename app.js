ab103dae4d856cc96169225e1af2b9488f5e4951
pthomas2001 <71702126+pthomas2001@users.noreply.github.com>
2021-03-12 12:37:20 +0100

    js

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 var canvas = document.getElementById('myCanvas');
 var context = canvas.getContext('2d');
+
+var size = 50;
+var positionX = 20;
+var positionY = 15;
+
+var paddingX = 20;
+var paddingY = 15;
+
+for (var i = 0; i < 15; i++) {
+  if((i + 1) % 3 === 0 && (i + 1) % 5 === 0) {
+    context.fillStyle = 'rgba(0,255,0,.5)';
+  }
+  else if ((i + 1) % 3 === 0) {
+    context.fillStyle = 'rgba(0,0,255,.5)';
+  }
+  else if ((i + 1) % 5 === 0) {
+    context.fillStyle = 'rgba(255,255,0,.5)';
+  }
+  else {
+    context.fillStyle = 'rgba(0,0,0,.5)';
+  }
+  context.fillRect(positionX, positionY, size, size);
+  positionX += paddingX;
+  positionY += paddingY;
+}
